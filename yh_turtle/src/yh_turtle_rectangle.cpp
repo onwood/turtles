16dec91091dec200c5d46de803b2d89dc7de8dd4
onwood <onwood93@gmail.com>
2021-06-15 17:47:40 +0900

    210615

--- yh_turtle/src/yh_turtle_rectangle.cpp
+++ yh_turtle/src/yh_turtle_rectangle.cpp
@@ -18,46 +18,37 @@
 
 int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "yh_turtle_circle");
+    ros::init(argc, argv, "yh_turtle_triangle");
 
     ros::NodeHandle nh;
     ros::Publisher pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 1000);
-    // ros::Publisher pub2 = nh.advertise<turtlesim::Pose>("/turtle1/pose", 1000);
-    //ros::ServiceClient client1 = nh.serviceClient<turtlesim::Spawn>("/turtlesim/Spawn");
     ros::ServiceClient client = nh.serviceClient<turtlesim::TeleportAbsolute>("/turtle1/teleport_absolute");
     ros::ServiceClient client_pen = nh.serviceClient<turtlesim::SetPen>("/turtle1/set_pen");
-    
     ros::Rate loop_rate(10);
-    int cnt = 0;
-    int r = atoll(argv[1]);
+    ros::Time start_time;
+    ros::Time end_time;
+
+    int cnt = 1;
+    int speed = atoll(argv[1]);
+    int distance = atoll(argv[2]);
     
-    // turtlesim::Pose p;
-    // p.x = 3;
-    // p.y = 3;
-    // p.theta = 0;
-    // pub2.publish(p);
-    // ROS_INFO("sent");
+    geometry_msgs::Twist t;
     
-    // turtlesim::Spawn srv;
-    // srv.request.x = 3.0;
-    // srv.request.y = 3.0;
-    // srv.request.theta = 0.0;
-    // if(client1.call(srv))
-    // {
-    //     ROS_INFO("sent");    
-    // }
-    // else //서버로 요청이 안 보내졌으면
-	// {
-	// 	ROS_ERROR("Failed to call service");
-	// 	return 1;
-	// }
+
+    // t.linear.y = 0;
+    // t.linear.z = 0;
+    // t.angular.x = 0;
+    // t.angular.y = 0;
+    
+
+
     turtlesim::SetPen pen;
     pen.request.off = 1;
     if (client_pen.call(pen))
     {
         turtlesim::TeleportAbsolute srv;
         srv.request.x = 5.544445;
-        srv.request.y = 5.544445 + r;
+        srv.request.y = 5.544445 + distance;
         srv.request.theta = 0;
         client.call(srv);
         pen.request.off = 0;
@@ -70,23 +61,37 @@ int main(int argc, char** argv)
 		return 1;
 	}
 
-    while(ros::ok())
-    {
-        if((cnt<4) | ((r == 1)&(cnt<4)))
+    while (ros::ok())
+    {        
+        if(cnt%2==0)
         {
-            geometry_msgs::Twist t;
-            t.angular.z = -(2 * 3.14);// 순수 각도
-            t.linear.x = 2 * 3.14 * r;//r * 쎄타
-            pub.publish(t);
-            ros::spinOnce();
+            t.angular.z = -22.0/7.0/2.0;
+            t.linear.x = 0;
         }
         else
         {
-            break;
+            t.angular.z = 0;
+            t.linear.x = speed;
+            // if (speed < 0)
+            // {
+            //     t.linear.x = -speed;
+            // }
+            // else
+            // {
+            //     t.linear.x = speed;
+            // }
         }
-        
-        ++cnt;
-        loop_rate.sleep();
+        start_time = ros::Time::now();
+        double current_distance = 0;
+        while(current_distance < distance)
+        {
+            pub.publish(t);
+            end_time = ros::Time::now();
+            ros::Duration duration = end_time - start_time;
+            double duration_sec = duration.toSec();
+            current_distance = speed*duration_sec;
+        }
+        cnt++;
     }
     return 0;
 }
